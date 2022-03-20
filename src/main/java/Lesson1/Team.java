45e2b0f4f39485d7705eda2385c2ef80859fa2e8
RomanUkolov <Roman_tora@mail.ru>
2022-03-20 17:44:35 +0300

    created class Team

---
+++ src/main/java/Lesson1/Team.java
@@ -0,0 +1,11 @@
+package Lesson1;
+
+public class Team {
+    private String nameTeam;
+    private Participant[] participants;
+
+    Team(String nameTeam, Participant[] participants) {
+        this.nameTeam = nameTeam;
+        this.participants = participants;
+    }
+}
