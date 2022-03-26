a4b78a09780e1fb611c6fe4ec3954edcbbab762f
RomanUkolov <Roman_tora@mail.ru>
2022-03-26 15:45:12 +0300

    create main class

--- src/main/java/Lesson2/Main.java
+++ src/main/java/Lesson2/Main.java
@@ -1,2 +1,14 @@
-package Lesson2;public class Main {
+package Lesson2;
+/**
+ * JavaCoreForQA_HomeWork
+ *
+ * @autor Roman Ukolov
+ * @version 27.03.2022
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
 }
