713f2a21f0d7b9072bda0ae185f01c27b53d0eaa
RomanUkolov <Roman_tora@mail.ru>
2022-03-31 21:12:50 +0300

    create main

---
+++ src/main/java/Lesson4/Main.java
@@ -0,0 +1,14 @@
+package Lesson4;
+/**
+ * JavaCoreForQA_HomeWork4
+ *
+ * @autor Roman Ukolov
+ * @version 31.03.2022
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
