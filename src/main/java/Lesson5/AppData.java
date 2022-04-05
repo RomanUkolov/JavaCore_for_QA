3cde80f38518faa733548e3b5b8f004c05cc01bf
Roman Ukolov <Roman_tora@mail.ru>
2022-04-05 16:17:03 +0300

    saveFile is works

--- src/main/java/Lesson5/AppData.java
+++ src/main/java/Lesson5/AppData.java
@@ -1,10 +1,18 @@
 package Lesson5;
+/**
+ * JavaCoreForQA_HomeWork4
+ *
+ * @autor Roman Ukolov
+ * @version 31.03.2022
+ *
+ */
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class AppData {
-    private String[] header = {"value1", "value2", "Value3"};
-    private int[][] data;
+    private String[] header = {"Value1", "Value2", "Value3"};
+    private int[][] data =  {{50, 74, 30}, {10, 28, 67}, {25, 99, 100}};
 
     public String[] getHeader() {
         return header;
@@ -26,11 +34,27 @@ public class AppData {
     }
 
     public  void saveFile(String fileName) {
-        try (PrintWriter printWriter = new PrintWriter(new FileWriter(fileName))) {
-            printWriter.write(String.valueOf(header));
+        /*
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(fileName)).) {
+            printWriter.write(Arrays.toString(header));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            for (int i = 0; i < header.length; i++) {
+                bufferedWriter.write(header[i] + "; ");
+            }
+            bufferedWriter.write("\n");
+
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; j < data[i].length; j++) {
+                    bufferedWriter.write(data[i][j] + "; ");
+                }
+                    bufferedWriter.write("\n");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
--- src/main/java/Lesson5/Main.java
+++ src/main/java/Lesson5/Main.java
@@ -1,9 +1,25 @@
 package Lesson5;
+/**
+ * JavaCoreForQA_HomeWork4
+ *
+ * @autor Roman Ukolov
+ * @version 31.03.2022
+ *
+ * 1. Реализовать сохранение данных в csv файл
+ * 2. Реализовать загрузку данных из csv файла. Файл читается целиком
+ *
+ *  Структура csv файла:
+ * | Строка заголовок с набором столбцов |
+ * | Набор строк с целочисленными значениями |
+ * | * Разделитель между столбцами - символ точка с запятой (;) |
+ */
 
 public class Main {
     public static void main(String[] args) {
         AppData appData = new AppData();
         appData.saveFile("test.cvs");
+
+
 
     }
 }
