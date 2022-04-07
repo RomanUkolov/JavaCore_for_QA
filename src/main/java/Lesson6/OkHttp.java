6b27baf018acc603d3df0484e87ba9dd3d276d62
Roman Ukolov <Roman_tora@mail.ru>
2022-04-07 16:18:46 +0300

    ..

--- src/main/java/Lesson6/OkHttp.java
+++ src/main/java/Lesson6/OkHttp.java
@@ -1,9 +1,20 @@
 package Lesson6;
 
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class OkHttp {
-    public static void main(String[] args) {
-        OkHttpClient client = new OkHttpClient();
+    public static void main(String[] args) throws IOException {
+        OkHttpClient okHttpClient = new OkHttpClient()
+                .newBuilder()
+                .readTimeout(10, TimeUnit.SECONDS)
+                .build();
+
+
+
     }
 }
