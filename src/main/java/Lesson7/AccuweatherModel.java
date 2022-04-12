52d971a281957a2ff8aec85c067c31bf00274db5
RomanUkolov <Roman_tora@mail.ru>
2022-04-12 21:05:49 +0300

    FIVE_DAY request  done

---
+++ src/main/java/Lesson7/AccuweatherModel.java
@@ -0,0 +1,108 @@
+package Lesson7;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class AccuweatherModel implements WeatherModel {
+    //http://dataservice.accuweather.com/forecasts/v1/daily/5day/
+    private static final String PROTOKOL = "https";
+    private static final String BASE_HOST = "dataservice.accuweather.com";
+    private static final String FORECASTS = "forecasts";
+    private static final String VERSION = "v1";
+    private static final String DAILY = "daily";
+    private static final String ONE_DAY = "1day";
+    private static final String FIVE_DAYS = "5day";
+    private static final String API_KEY = "JjXbjKVkpUpgdQAtJtAHFQvlz5GAM6YL";
+    private static final String API_KEY_QUERY_PARAM = "apikey";
+    private static final String API_KEY_LANG_QUERY_PARAM ="language";
+    private static final String API_LANGUAGE = "ru-ru";
+    private static final String API_KEY_METRIC_QUERY_PARAM = "metric";
+    private static final boolean API_METRIC = true;
+    private static final String LOCATIONS = "locations";
+    private static final String CITIES = "cities";
+    private static final String AUTOCOMPLETE = "autocomplete";
+
+    private static final OkHttpClient okHttpClient = new OkHttpClient();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    public void getWeather(String selectedCity, Period period) throws IOException {
+        switch (period) {
+            case NOW:
+                HttpUrl httpUrl = new HttpUrl.Builder()
+                        .scheme(PROTOKOL)
+                        .host(BASE_HOST)
+                        .addPathSegment(FORECASTS)
+                        .addPathSegment(VERSION)
+                        .addPathSegment(DAILY)
+                        .addPathSegment(ONE_DAY)
+                        .addPathSegment(detectCityKey(selectedCity))
+                        .addQueryParameter(API_KEY_QUERY_PARAM, API_KEY)
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url(httpUrl)
+                        .build();
+
+                Response oneDayForecastResponse = okHttpClient.newCall(request).execute();
+                String weatherResponse = oneDayForecastResponse.body().string();
+                System.out.println(weatherResponse);
+                break;
+            case FIVE_DAYS:
+                HttpUrl httpUrl1 = new HttpUrl.Builder()
+                        .scheme(PROTOKOL)
+                        .host(BASE_HOST)
+                        .addPathSegment(FORECASTS)
+                        .addPathSegment(VERSION)
+                        .addPathSegment(DAILY)
+                        .addPathSegment(FIVE_DAYS)
+                        .addPathSegment(detectCityKey(selectedCity))
+                        .addQueryParameter(API_KEY_QUERY_PARAM, API_KEY)
+                        .addQueryParameter(API_KEY_LANG_QUERY_PARAM, API_LANGUAGE)
+                        .addQueryParameter(API_KEY_METRIC_QUERY_PARAM, String.valueOf(API_METRIC))
+                        .build();
+
+                Request requestFiveDay = new Request.Builder()
+                        .url(httpUrl1)
+                        .build();
+
+                Response fiveDayForecastResponse = okHttpClient.newCall(requestFiveDay).execute();
+                String weatherResponseFiveDay = fiveDayForecastResponse.body().string();
+                System.out.println(weatherResponseFiveDay);
+
+                break;
+        }
+    }
+
+    private String detectCityKey(String selectCity) throws IOException {
+        //http://dataservice.accuweather.com/locations/v1/cities/autocomplete
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme(PROTOKOL)
+                .host(BASE_HOST)
+                .addPathSegment(LOCATIONS)
+                .addPathSegment(VERSION)
+                .addPathSegment(CITIES)
+                .addPathSegment(AUTOCOMPLETE)
+                .addQueryParameter(API_KEY_QUERY_PARAM, API_KEY)
+                .addQueryParameter("q", selectCity)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(httpUrl)
+                .get()
+                .addHeader("accept", "application/json")
+                .build();
+
+        Response response = okHttpClient.newCall(request).execute();
+        String responseString = response.body().string();
+
+        String cityKey = objectMapper.readTree(responseString).get(0).at("/Key").asText();
+
+        return cityKey;
+    }
+}
---
+++ src/main/java/Lesson7/Main.java
@@ -0,0 +1,24 @@
+package Lesson7;
+/**
+ * JavaCoreForQA_HomeWork4
+ *
+ * @autor Roman Ukolov
+ * @version 11.04.2022
+ *
+ */
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        AccuweatherModel accuweatherModel = new AccuweatherModel();
+        accuweatherModel.getWeather("Moscow", Period.NOW);
+
+
+    }
+}
